aeecadd08b422a93632511419b1c1d82d08ff1d0
Strahil Hristov <strahil1998@abv.bg>
2023-11-10 16:04:58 +0200

    helper

---
+++ lottery_project/src/Helper/actions.js
@@ -0,0 +1,28 @@
+import { getRandomNumber, getRandomColor } from "./utils";
+
+export function registerTicket() {
+    const newTicket = {
+        number: getRandomNumber(),
+        color: getRandomColor()
+    }
+
+    this.setState((prevState) => {
+        prevState.tickets.push(newTicket);
+
+        return {
+            tickets: prevState.tickets,
+            remainingTickets: --prevState.remainingTickets
+        }
+    })
+}
+
+export function removeTicket(index) {
+    this.setState((prevState) => {
+        prevState.tickets.splice(index, 1);
+
+        return {
+            tickets: prevState.tickets,
+            remainingTickets: ++prevState.remainingTickets
+        }
+    });
+}
