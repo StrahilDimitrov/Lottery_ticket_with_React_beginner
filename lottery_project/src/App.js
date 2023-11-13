37901d7e6b9d61cdbb7b4a0789c7394c0505c2fc
Strahil Hristov <strahil1998@abv.bg>
2023-11-13 21:12:14 +0200

    update app

--- lottery_project/src/App.js
+++ lottery_project/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { getRandomNumber } from "./Helper/utils";
-import { registerTicket } from "./Helper/actions";
+import { registerTicket, removeTicket, finish, reset } from "./Helper/actions";
 import Lottery from "./components/Lottery";
+import Final from "./components/Final";
 import './App.css';
 
 class App extends Component {
@@ -16,16 +17,32 @@ class App extends Component {
     };
 
     this.registerTicket = registerTicket.bind(this);
+    this.removeTicket = removeTicket.bind(this);
+    this.finish = finish.bind(this);
+    this.reset = reset.bind(this);
   }
 
   renderApp() {
-    const {tickets, remaningTickets} = this.state;
+    const { tickets, remaningTickets, finished, winningNumber } = this.state;
     const actions = {};
+
+    if (finished) {
+      actions.reset = this.reset;
+      return <Final
+        actions={actions}
+        tickets={tickets}
+        winningNumber={winningNumber}
+      />
+    }
+
     actions.registerTicket = this.registerTicket;
+    actions.removeTicket = this.removeTicket;
+    actions.finish = this.finish;
+
     return (
       <Lottery
         actions={actions}
-        tickets = {tickets}
+        tickets={tickets}
         remaningTickets={remaningTickets}
       />
     )
